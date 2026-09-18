1c7ff7a62e091d8dd3e49497f689840d19c0aae3
Snn1452 <snn1452@gmail.com>
2026-09-19 01:38:20 +0300

    Add mail background host identity

--- src/Wino.NotificationHost.Contracts/NotificationHostApplicationIds.cs
+++ src/Wino.NotificationHost.Contracts/NotificationHostApplicationIds.cs
@@ -8,6 +8,7 @@ public static class NotificationHostApplicationIds
     public const string People = "PeopleNotificationHost";
     public const string Tasks = "ToDoNotificationHost";
     public const string Background = "MailBackgroundHost";
+    public const string Background = "MailBackgroundHost";
 
     public static string GetApplicationId(NotificationHostApplication application)
         => application switch
