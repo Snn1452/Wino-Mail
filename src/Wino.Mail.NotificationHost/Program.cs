3cbf5022620caa2b95a55ab31b0cb83e2f26caa6
Snn1452 <snn1452@gmail.com>
2026-09-19 13:02:44 +0300

    Route mail background application to mail-specific host runtime

--- src/Wino.Mail.NotificationHost/Program.cs
+++ src/Wino.Mail.NotificationHost/Program.cs
@@ -1,8 +1,32 @@
 using System;
+using Windows.ApplicationModel;
 using Wino.NotificationHost;
+using Wino.NotificationHost.Contracts;
 
 internal static class Program
 {
     [STAThread]
-    private static int Main(string[] args) => NotificationHostRuntime.Run(args);
+    private static int Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        if (IsBackgroundHost())
+        {
+            var package = Package.Current;
+            ReleaseIdentity.Initialize(
+                package.InstalledLocation.Path,
+                package.Id.Name,
+                package.Id.Publisher,
+                package.Id.FamilyName);
+
+            return BackgroundHostRuntime.Run();
+        }
+
+        return NotificationHostRuntime.Run(args);
+    }
+
+    private static bool IsBackgroundHost()
+        => CurrentAppIdentity.GetAppUserModelId().EndsWith(
+            "!" + NotificationHostApplicationIds.Background,
+            StringComparison.Ordinal);
 }
