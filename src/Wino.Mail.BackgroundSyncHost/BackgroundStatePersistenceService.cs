7b4cd6f362a31b16a34f19a346c6811405e62a73
Snn1452 <snn1452@gmail.com>
2026-09-19 22:48:12 +0300

    Add background host component BackgroundStatePersistenceService.cs

---
+++ src/Wino.Mail.BackgroundSyncHost/BackgroundStatePersistenceService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel;
+using Wino.Core.Domain.Enums;
+using Wino.Core.Domain.Interfaces;
+
+namespace Wino.Mail.BackgroundSyncHost;
+
+internal sealed class BackgroundStatePersistenceService : IStatePersistanceService
+{
+    public event EventHandler<string>? StatePropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public bool IsReadingMail { get; set; }
+    public string CoreWindowTitle { get; set; } = string.Empty;
+    public string AppModeTitle { get; set; } = "Wino Mail";
+    public bool IsReaderNarrowed { get; set; }
+    public WinoApplicationMode ApplicationMode { get; set; } = WinoApplicationMode.Mail;
+    public bool IsEventDetailsVisible { get; set; }
+    public double OpenPaneLength { get; set; }
+    public bool ShouldShiftMailRenderingDesign { get; set; }
+    public double MailListPaneLength { get; set; }
+    public CalendarDisplayType CalendarDisplayType { get; set; } = CalendarDisplayType.Week;
+    public int DayDisplayCount { get; set; } = 1;
+}
