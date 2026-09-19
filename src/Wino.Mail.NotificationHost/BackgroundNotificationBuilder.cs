5f5acc79a43daa3c192b742be61d18f2131aa245
Snn1452 <snn1452@gmail.com>
2026-09-19 22:16:50 +0300

    Fix background host compilation imports

--- src/Wino.Mail.NotificationHost/BackgroundNotificationBuilder.cs
+++ src/Wino.Mail.NotificationHost/BackgroundNotificationBuilder.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Windows.AppNotifications;
 using Microsoft.Windows.AppNotifications.Builder;
 using Wino.Core.Domain;
