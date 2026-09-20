a2babae34f967e1d58957d4bf3c261f94c7e2f09
Snn1452 <snn1452@gmail.com>
2026-09-20 03:32:12 +0300

    Remove temporary Magika diagnostics from smoke test

--- tests/Wino.Services.AotSmoke/Program.cs
+++ tests/Wino.Services.AotSmoke/Program.cs
@@ -105,7 +105,6 @@ try
 
     using var contentTypeModel = new MagikaContentTypeClassificationModel();
     await using var contentTypeStream = new MemoryStream("Native AOT content type smoke test"u8.ToArray());
-    Environment.SetEnvironmentVariable("WINO_MAGIKA_DIAGNOSTICS", "1");
     var classification = await contentTypeModel.ClassifyAsync(contentTypeStream).ConfigureAwait(false);
 
     if (contentTypeModel.IsSupported && classification.Label != "txt")
