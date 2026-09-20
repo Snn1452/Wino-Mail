ddc5780567711a85582168fc0f685440975e1f10
Snn1452 <snn1452@gmail.com>
2026-09-20 03:34:20 +0300

    Register legacy encodings for headless mail sync

--- src/Wino.Mail.BackgroundSyncHost/Program.cs
+++ src/Wino.Mail.BackgroundSyncHost/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,6 +29,7 @@ internal static class Program
     private static int Main()
     {
         WinRT.ComWrappersSupport.InitializeComWrappers();
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
         try
         {
