3b80cad6a1c3c92c96ec804918c8cf94e70ec6fc
Snn1452 <snn1452@gmail.com>
2026-09-22 02:50:59 +0300

    Stop calendar reminder host when background mode is disabled

--- src/Wino.Mail.BackgroundSyncHost/CalendarReminderService.cs
+++ src/Wino.Mail.BackgroundSyncHost/CalendarReminderService.cs
@@ -3,12 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Wino.Core.Domain.Enums;
 using Wino.Core.Domain.Interfaces;
+
 namespace Wino.Mail.BackgroundSyncHost;
-internal sealed class CalendarReminderService(ICalendarService calendarService,IAccountService accountService,INotificationBuilder notificationBuilder)
+
+internal sealed class CalendarReminderService(
+    ICalendarService calendarService,
+    IAccountService accountService,
+    IPreferencesService preferencesService,
+    INotificationBuilder notificationBuilder)
 {
-    private static readonly TimeSpan Interval=TimeSpan.FromSeconds(30);
-    private readonly HashSet<string> _keys=[];
-    private DateTime _last=DateTime.Now.AddSeconds(-30);
-    public async Task RunAsync(CancellationToken t){using var timer=new PeriodicTimer(Interval);while(await timer.WaitForNextTickAsync(t)){var now=DateTime.Now;var accounts=await accountService.GetAccountsAsync();if(accounts.Any(a=>a.IsCalendarAccessGranted)){var due=await calendarService.CheckAndNotifyAsync(_last,now,_keys,t);foreach(var item in due)await notificationBuilder.CreateCalendarReminderNotificationAsync(item.CalendarItem,item.ReminderDurationInSeconds);} _last=now;}}
+    private static readonly TimeSpan Interval = TimeSpan.FromSeconds(30);
+    private readonly HashSet<string> _keys = [];
+    private DateTime _last = DateTime.Now.AddSeconds(-30);
+
+    public async Task RunAsync(CancellationToken token)
+    {
+        using var timer = new PeriodicTimer(Interval);
+
+        while (await timer.WaitForNextTickAsync(token).ConfigureAwait(false))
+        {
+            if (preferencesService.AppCloseBehavior == AppCloseBehavior.Terminate)
+                return;
+
+            var now = DateTime.Now;
+            var accounts = await accountService.GetAccountsAsync().ConfigureAwait(false);
+
+            if (accounts.Any(account => account.IsCalendarAccessGranted))
+            {
+                var due = await calendarService
+                    .CheckAndNotifyAsync(_last, now, _keys, token)
+                    .ConfigureAwait(false);
+
+                foreach (var item in due)
+                {
+                    await notificationBuilder
+                        .CreateCalendarReminderNotificationAsync(
+                            item.CalendarItem,
+                            item.ReminderDurationInSeconds)
+                        .ConfigureAwait(false);
+                }
+            }
+
+            _last = now;
+        }
+    }
 }
