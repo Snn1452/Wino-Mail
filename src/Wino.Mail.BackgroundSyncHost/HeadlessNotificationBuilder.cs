39f40435739564fda8ca7cfa4fe631ff8fbf6115
Snn1452 <snn1452@gmail.com>
2026-09-21 18:08:20 +0300

    Use notification builder string icon API

--- src/Wino.Mail.BackgroundSyncHost/HeadlessNotificationBuilder.cs
+++ src/Wino.Mail.BackgroundSyncHost/HeadlessNotificationBuilder.cs
@@ -245,7 +245,7 @@ internal sealed class HeadlessNotificationBuilder(
                 builder.AddComboBox(selectionBox);
                 builder.AddButton(
                     new AppNotificationButton(Translator.CalendarReminder_SnoozeAction)
-                        .SetIcon(new Uri(GetNotificationIconUri("calendar-snooze")))
+                        .SetIcon(GetNotificationIconUri("calendar-snooze"))
                         .AddArgument(Constants.ToastCalendarActionKey, Constants.ToastCalendarSnoozeAction)
                         .AddArgument(Constants.ToastCalendarItemIdKey, calendarItem.Id.ToString())
                         .AddArgument(Constants.ToastModeKey, Constants.ToastModeCalendar));
@@ -261,7 +261,7 @@ internal sealed class HeadlessNotificationBuilder(
             {
                 builder.AddButton(
                     new AppNotificationButton(Translator.CalendarEventDetails_JoinOnline)
-                        .SetIcon(new Uri(GetNotificationIconUri("calendar-join")))
+                        .SetIcon(GetNotificationIconUri("calendar-join"))
                         .AddArgument(Constants.ToastCalendarActionKey, Constants.ToastCalendarJoinOnlineAction)
                         .AddArgument(Constants.ToastCalendarItemIdKey, calendarItem.Id.ToString())
                         .AddArgument(Constants.ToastModeKey, Constants.ToastModeCalendar));
